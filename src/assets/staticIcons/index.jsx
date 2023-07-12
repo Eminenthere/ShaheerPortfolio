bea3de315329e851f8e9fe60734c034b15ccec2c
eminentishere <eminentishere@outlook.com>
2023-07-12 10:06:23 +0500

    new section DSA added

--- src/assets/staticIcons/index.jsx
+++ src/assets/staticIcons/index.jsx
@@ -1,7 +1,19 @@
-import GithubIcon from "./Github-Original.svg";
-import WatchIcon from "./clock-stopwatch.svg";
-import Guard from "./guard.svg";
-import DesignIcon from "./Design.svg";
-import BoxIcon from "./boxIcon.svg";
+import GithubIcon from './Github-Original.svg'
+import WatchIcon from './clock-stopwatch.svg'
+import Guard from './guard.svg'
+import DesignIcon from './Design.svg'
+import BoxIcon from './boxIcon.svg'
+import LeetCodeIcon from './leetCodeIcon.svg'
+import ComplexityIcon from './compexityIcon.svg'
+import BigOIcon from './bigOIcon.svg'
 
-export { GithubIcon, WatchIcon, Guard, DesignIcon, BoxIcon };
+export {
+  GithubIcon,
+  WatchIcon,
+  Guard,
+  DesignIcon,
+  BoxIcon,
+  LeetCodeIcon,
+  ComplexityIcon,
+  BigOIcon,
+}
--- src/components/DataStructureAlgorithm.jsx
+++ src/components/DataStructureAlgorithm.jsx
@@ -1,20 +1,50 @@
-import { BoxIcon } from '../assets/staticIcons'
+import {
+  WatchIcon,
+  LeetCodeIcon,
+  ComplexityIcon,
+  BigOIcon,
+} from '../assets/staticIcons'
 import '../styles/DataStructureAlgorithm.css'
+import SecondaryBtn from './SecondaryBtn'
+import { DataStructureArray } from '../data'
 
 const DataStructureAlgorithm = () => {
   return (
     <section className='dsa-wrapper'>
-      <div className='dsa-grid'></div>
+      <h1 className='text-48-600'>Data Structures & Algorithms</h1>
+      <div className='dsa-grid'>
+        {DataStructureArray.map((question) => {
+          const {
+            dsaQuestionTitle,
+            dsaQuestion,
+            dsaSolution,
+            dsaDuration,
+            complexity,
+            bigO,
+            id,
+          } = question
+          return (
+            <div className='dsa-card-container' key={id}>
+              <div className='dsa-title-container'>
+                <p className='text-36-600'>{dsaQuestionTitle}</p>
+                <p>{dsaQuestion}</p>
+              </div>
 
-      <div className='dsa-card-container'>
-        <div className='dsa-title-container'>
-          <img src={BoxIcon} alt='Algorithm and DataStructure icon' />
-          <p className='text-36-600'>Two Sum</p>
-          <p>
-            Given an array of integers nums and an integer target, return
-            indices of the two numbers such that they add up to target.
-          </p>
-        </div>
+              <div className='buttons-row'>
+                <div className='info-tag'>
+                  <SecondaryBtn icon={WatchIcon} value={dsaDuration} />
+                  <SecondaryBtn icon={ComplexityIcon} value={complexity} />
+                  <SecondaryBtn icon={BigOIcon} value={bigO} />
+                </div>
+                <SecondaryBtn
+                  icon={LeetCodeIcon}
+                  value={'LeetCode Solution'}
+                  link={dsaSolution}
+                />
+              </div>
+            </div>
+          )
+        })}
       </div>
     </section>
   )
